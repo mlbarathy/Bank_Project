ae25d14f7375d74c09e41869eab6d951a6605838
lakshimi.mariappan <lakshimi.mariappan@almosafer.com>
2025-05-29 23:10:36 +0400

    Working Code - Postgress Write

--- app/datatypes.py
+++ app/datatypes.py
@@ -1,26 +1,17 @@
 from pyspark.sql.types import *
 
-def spark_to_postgres_type(data_type):
-    if isinstance(data_type, StringType):
-        return "TEXT"
-    elif isinstance(data_type, IntegerType):
-        return "INTEGER"
-    elif isinstance(data_type, LongType):
-        return "BIGINT"
-    elif isinstance(data_type, DoubleType):
-        return "DOUBLE PRECISION"
-    elif isinstance(data_type, BooleanType):
-        return "BOOLEAN"
-    elif isinstance(data_type, TimestampType):
-        return "TIMESTAMP"
-    return "TEXT"
 
-def spark_to_postgres_type(py_type):
-    if py_type == int:
-        return "INTEGER"
-    elif py_type == float:
-        return "DOUBLE PRECISION"
-    elif py_type == bool:
-        return "BOOLEAN"
-    else:
-        return "TEXT"
+
+def spark_to_postgres_type(spark_type):
+
+    mapping = {
+        StringType(): "TEXT",
+        IntegerType(): "INTEGER",
+        LongType(): "BIGINT",
+        DoubleType(): "DOUBLE PRECISION",
+        FloatType(): "REAL",
+        BooleanType(): "BOOLEAN",
+        TimestampType(): "TIMESTAMP",
+        DateType(): "DATE",
+    }
+    return mapping.get(spark_type, "TEXT")
